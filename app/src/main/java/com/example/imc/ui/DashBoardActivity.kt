831a28e8a982379aefdd3ef2f629d2724315b239
PedroMonteiro7 <pedromonteirogt@gmail.com>
2021-12-06 11:24:04 -0300

    Trabalhando no histórico de pesagens

--- app/src/main/java/com/example/imc/ui/DashBoardActivity.kt
+++ app/src/main/java/com/example/imc/ui/DashBoardActivity.kt
@@ -3,13 +3,16 @@ package com.example.imc.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import com.example.imc.PesoActivity
 import com.example.imc.R
 import com.example.imc.RegistrarNovoPeso
+import com.example.imc.repository.PesagemRepository
 import com.example.imc.utils.calcularIdade
 import com.example.imc.utils.convertBase64ToBitmap
 
@@ -24,6 +27,7 @@ class DashBoardActivity : AppCompatActivity() {
     lateinit var tvAltura: TextView
     lateinit var ivPerfil: ImageView
     lateinit var cardNovaPesagem: CardView
+    lateinit var cardHistorico: CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,10 +51,25 @@ class DashBoardActivity : AppCompatActivity() {
         tvImc = findViewById(R.id.tv_dash_imc)
         ivPerfil = findViewById(R.id.iv_dash_foto_perfil)
         cardNovaPesagem = findViewById(R.id.cardViewPesar)
+        cardHistorico = findViewById(R.id.card_historico)
 
         cardNovaPesagem.setOnClickListener {
             val intent = Intent(this, PesoActivity::class.java)
             startActivity(intent)
+        }
+
+        cardHistorico.setOnClickListener {
+
+            val intent = Intent(this, HistoricoActivity::class.java)
+
+//            val pesagemRepository = PesagemRepository(this)
+//            val listaPesagem = pesagemRepository.getListaPesagem()
+//
+//            for (p in listaPesagem) {
+//                Log.i("xpto", "${p.dataPesagem} - ${p.peso}")
+//            }
+
+            Toast.makeText(this, "teste", Toast.LENGTH_SHORT)
         }
 
         carregarDashboard()
